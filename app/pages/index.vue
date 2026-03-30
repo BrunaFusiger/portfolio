11b33da658f1d0dfe53c5bf294915b3edd40dd75
brunaFusiger <bruna@zytro.co.uk>
2026-03-30 15:07:30 +0200

    Update z-index for decorative element in index.vue to improve layering and visual hierarchy

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="relative isolate w-full">
       <div
-        class="pointer-events-none absolute z-0 hidden h-80 w-80 -left-50 top-30 lg:block"
+        class="pointer-events-none absolute z-10 hidden h-80 w-80 -left-50 top-30 lg:block"
         aria-hidden="true"
       >
         <TornPaperDecor side="left" />
